f69e12b98fde2f87398d41c9bb9a1536b2b43d3d
Ahmet Bozaci <ahmt9417@gmail.com>
2022-02-14 17:56:34 +0300

    Install react-router, update components

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,12 @@
-const App = () => <div>App component</div>
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Greeting from './Greeting';
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Greeting />} />
+    </Routes>
+  </Router>
+)
 
 export default App;
---
+++ src/components/Greeting.js
@@ -0,0 +1,3 @@
+const Greeting = () => <div>Greeting Compoent</div>
+
+export default Greeting;

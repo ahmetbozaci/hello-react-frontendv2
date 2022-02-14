12d59ce85cbeca66f6919ff360e6e45d506e1ae2
Ahmet Bozaci <ahmt9417@gmail.com>
2022-02-14 18:49:36 +0300

    Style greeting component

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,13 +1,12 @@
-import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import getGreetingData from "../redux/API";
 import store from "../redux/configureStore";
-
+import './Greeting.css';
 store.dispatch(getGreetingData)
 const Greeting = () =>{
   const greeting = useSelector((state) => state.greetingData);
   return(
-    <div>{greeting}</div>
+    <p>{greeting}</p>
   )
 }
 
